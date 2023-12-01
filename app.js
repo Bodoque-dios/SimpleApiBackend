bffd6912d7c982a100c99dca198301a5e21f1eea
Matias Olea <mati.olea.thomsen@gmail.com>
2023-11-30 21:40:52 -0300

    added landing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const port =  process.env.PORT || 42069;
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static('public'))
 
 app.use("/api/users", users);
 app.use("/api/blinds", blinds);
